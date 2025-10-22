ed18eacee6ef2ffb50fbe54ffef19f7cd49a4c3c
Anand Prakash <anandkp92@gmail.com>
2025-10-22 11:25:40 -0700

    adding cxf translation and JS translation

---
+++ normal_translation/Controls/Subsequences/temDifSelectionMin.js
@@ -0,0 +1,19 @@
+
+// http://example.org#cdl_models.Controls.Subsequences.temDifSelectionMin
+
+
+module.exports = (
+  {
+		nZones = 3,
+    } = {}
+) => {
+
+
+  return (
+    {  }
+  ) => {
+
+
+    return {};
+  }
+}
